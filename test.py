0ff053e10748288d89c5b737e36bfa3362e9373c
khaerilanwar <khaerilanwar1992@gmail.com>
2024-11-06 20:08:27 +0700

    update laptop

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 from dotenv import load_dotenv
 from lib.driver import WebDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
 
@@ -17,22 +18,45 @@ driver.authentication(url_login, email, password)
 time.sleep(4)
 
 # Masuk ke halaman feed grup
-driver.get("https://web.facebook.com/groups/feed/")
+driver.get("https://web.facebook.com/groups/784838364941674")
 time.sleep(3)
 
 # scroll ke bawah
-for i in range(2):
+for i in range(4):
     driver.scroll()
     time.sleep(4)
 
 feed_grup = driver.find_element(By.CSS_SELECTOR, 'div[role="feed"]')
 feed_posts = feed_grup.find_elements(By.XPATH, './div')
 
+count_post = 0
 for post in feed_posts:
-    # text_post = post.find_element(By.CSS_SELECTOR, 'div[data-ad-rendering-role="message"]').text
-    # print(text_post)
-    print(post.text)
-    print("\n", 50 * "-", "\n")
+    try:
+        # mendapatkan tombol komentar
+        button_comment = post.find_element(By.CSS_SELECTOR, 'div[aria-label="Beri komentar"][role="button"]')
+        button_comment.click()
+
+        # pindah ke input komentar
+        input_comment = driver.switch_to.active_element
+        input_comment.send_keys('Jasa pembuatan surat lamaran email dan CV menarik ..')
+        input_comment.send_keys(Keys.SHIFT, Keys.ENTER)
+        input_comment.send_keys('Harga mulai dari 5.000, pemesanan hanya melalui')
+        input_comment.send_keys(Keys.SHIFT, Keys.ENTER)
+        input_comment.send_keys('Whatsapp : 0821 3763 3527')
+        input_comment.send_keys(Keys.SHIFT, Keys.ENTER)
+        input_comment.send_keys('https://api.whatsapp.com/send?phone=6282137633527')
+        input_comment.send_keys(Keys.ENTER)
+
+        count_post += 1
+        print(f"Berhasil komentar ke {count_post}")
+
+        time.sleep(30)
+    except:
+        continue
+
+else:
+    print(len(feed_posts))
+    print(count_post)
 
 time.sleep(8)
 

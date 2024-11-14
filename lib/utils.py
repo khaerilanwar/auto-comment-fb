8d7e9f680b411f0dbd9203ecf1fd0c51685d88cc
khaerilanwar <khaerilanwar1992@gmail.com>
2024-11-14 11:53:33 +0700

    change locale usage

--- lib/utils.py
+++ lib/utils.py
@@ -1,9 +1,16 @@
-import locale
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 def get_today():
-    locale.setlocale(locale.LC_TIME, 'ind')
-    today = datetime.today()
-    today_translate = today.strftime("%A, %d %B %Y Pukul %H:%M")
+    # utc_now = datetime.utcnow()
+    # tz = timezone(timedelta(hours=7))
+    # actual_time = utc_now.astimezone(tz)
+    actual_time = datetime.now()
+
+    hari_dalam_minggu = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"]
+    bulan_dalam_tahun = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
+
+    hari = hari_dalam_minggu[actual_time.weekday()]
+    bulan = bulan_dalam_tahun[actual_time.month - 1]
+    today_translate = f"{hari}, {actual_time.day} {bulan}, {actual_time.year} Pukul {actual_time.hour}:{actual_time.minute}"
 
     return today_translate
